c1c92efc08baace8559990e93d99e7d649a360b3
Xcristian007X <74797095+Xcristian007X@users.noreply.github.com>
2022-04-19 07:28:10 -0400

    Add files via upload

---
+++ prueba texto golang/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net"
+)
+
+const addr = "127.0.0.1:3000"
+const bufferSize = 256
+const endLine = 10
+
+var clients []net.Conn
+
+func main() {
+	clients = make([]net.Conn, 0)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal("No se pudo escuchar en " + addr)
+	}
+
+	for {
+		conn, _ := listener.Accept()
+		clients = append(clients, conn)
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	var data []byte
+	buffer := make([]byte, bufferSize)
+
+	for {
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+			}
+			buffer = bytes.Trim(buffer[:n], "\x00")
+			data = append(data, buffer...)
+			if data[len(data)-1] == endLine {
+				break
+			}
+		}
+		sendToOtherClients(conn, data)
+		data = make([]byte, 0)
+	}
+}
+
+func sendToOtherClients(sender net.Conn, data []byte) {
+	for i := 0; i < len(clients); i++ {
+		if clients[i] != sender {
+			clients[i].Write((data))
+		}
+	}
+}
