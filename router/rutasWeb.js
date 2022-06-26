09c57ccabcfb9f7bba3b5cfd59fa6e575da72617
JoseAlderete <jalderete2020@alu.uct.cl>
2022-06-26 02:38:17 -0400

    final

---
+++ router/rutasWeb.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const router = express.Router();
+
+//modelo de base de datos
+const Foro = require('../models/foro')
+const Comentario = require('../models/comentario')
+
+//login, revisar ControlUser que es la funcionalidad///////////////////////////////////////////////////////////////////////////////
+const {renderSignUpForm, renderLoginForm, signup, login, logout} = require('../controllers/controlUsers')
+
+router.get("/signup", renderSignUpForm);
+
+router.post("/signup", signup);
+
+router.get("/", renderLoginForm);
+
+router.post("/", login);
+
+router.get("/logout", logout);
+
+/////////////////////////////////////////////////////////////////////////////////////
+router.get("/inicio", async (req, res) => {
+    try{
+        const error = "";
+        const success = "";
+        //Verifica si el usuario esta logeado o no////////////////////////////////////
+        if(!res.locals.user) {
+            res.status(401);
+            res.render('login', {
+                error: 'Se necesita haber iniciado sesion para ver el contenido'
+            })
+            res.end('no autorizado');
+        } else {
+        const arrayForosDB = await Foro.find();
+        //console.log(arrayForosDB)
+        res.render("index", {
+            titulo : "TwinSanity",
+            arrayForos: arrayForosDB,
+            username: res.locals.user
+        })}
+    } catch (error) {
+        console.log(error)
+    }
+  })
+
+  router.get("/foro/:id", async (req,res) => {
+
+      const id = req.params.id
+      try{
+        const error = "";
+        const success = "";
+        if(!res.locals.user) {
+            res.status(401);
+            res.render('login', {
+                success,
+                error: 'Se necesita haber iniciado sesion para ver el contenido'
+            })
+            res.end('no autorizado');
+        } else {
+        const ForosDB = await Foro.findOne({_id: id})
+        const arrayComentarioDB = await Comentario.find({id2: id});
+        res.render('detalle', {
+            comentario: arrayComentarioDB,
+            foro: ForosDB,
+            error: false,
+        })}
+      } catch (error){
+          res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el foro'
+        })
+      }
+  })
+
+router.post("/foro/:id", async(req, res) => {
+    const id = req.params.id
+    const body = req.body
+    try {
+        await Comentario.create(body)
+        res.redirect("/foro/"+id)
+    } catch (error){
+        console.log(error)
+    }
+   
+})
+
+router.post('/inicio', async(req, res) => {
+    const body = req.body
+    try {
+        await Foro.create(body)
+
+        res.redirect('/inicio')
+    } catch (error){
+        console.log(error)
+    }
+   
+})
+
+//Archivo pruebas tecnicas
+router.get("/prueba", (req, res) => {
+    res.render("prueba",{titulo : "Iniciar Sesion"});
+});
+
+
+//Extras
+
+router.get("/about", (req, res) => {
+    res.render("about",{titulo : "Quienes Somos"});
+});
+  
+router.get("/contact", (req, res) => {
+    res.render("contact",{titulo : "Contactanos"});
+});
+ 
+//VideoChat
+
+router.get("/:room", (req, res) => {
+    res.render("room",{titulo : "VideoChat", roomId: req.params.room});
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,113 @@
+//constantes y requerimientos
+require('./config/passport');
+const express = require("express");
+const bodyParser = require('body-parser')
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const flash = require('connect-flash');
+const session = require('express-session');1
+const passport = require('passport');
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+
+
+// Conexion a la Base de Datos
+const mongoose = require('mongoose')
+
+const pass = 'HOUzT8lr8l8ywPQT';
+const dbName = "foros";
+const uri = `mongodb+srv://root:${pass}@twinsanity.2ovgpc1.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+mongoose.connect(uri,
+  {useNewUrlParser: true, useUnifiedTopology: true }
+)
+  .then(() => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
+
+// llamado de dependencias
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use("/peerjs", peerServer);
+app.set("view engine", "ejs");
+app.set('views', __dirname + '/views');
+
+
+app.use(express.static( __dirname + "/public"));
+
+
+//Login//////////////////////////////////////////
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+//Variables Globales, solo esta en uso error, hay que probar user//////
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null
+  next();
+})
+
+
+//rutas WEB
+app.use('/', require('./router/rutasWeb'));
+
+
+app.get("/room", (req, res) => {
+  res.redirect(`/room/${uuidv4()}`);
+});
+
+
+app.use((req, res, next) => {
+    res.status(404).render("404", {
+        titulo: "404",
+        descripccion: "TwinSanity"
+    });
+});
+
+
+//Funciones para el servidor
+
+io.on("connection", (socket) => {
+
+  socket.on("join-room", (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
+  });
+
+  socket.on("chat", (msg, user) => {
+    io.emit("crearmsg", msg, user);
+  });
+
+  
+
+
+
+});
+
+
+
+server.listen(process.env.PORT || 3000);
